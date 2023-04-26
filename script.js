414d1bdd359efb88c2fe47a791f7885a5eacd041
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-26 13:44:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ const apiUrls = [
 ];
 
 // You can write your code here
+let start = Date.now();
+// apiUrls.forEach((endpoint)=>{
+// 	fetchAll(endpoint);
+// });
+
+async function fetchAll(endpoint){
+	try{
+		const res = await Promise.all(apiUrls.forEach((endpoint)=>{
+			fetchAll(endpoint);
+		}));
+		let data = await Promise.all(res.map(r=>{r.json()})); 
+	}
+	catch{
+		throw Error("Promise failed");
+	}
+}
+let timetaken = Date.now() - start;
+
+let pall = document.getElementById("output-all");
+pall.innerText = timetaken; 
+
+start = Date.now();
+async function fetchAll(endpoint){
+	try{
+		const res = await Promise.any(apiUrls.forEach((endpoint)=>{
+			fetchAll(endpoint);
+		}));
+		let data = await Promise.any(res.map(r=>{r.json()})); 
+	}
+	catch{
+		throw Error("Promise failed");
+	}
+}
+timetaken = Date.now() - start;
+
+ let pany = document.getElementById("output-any");
+pany.innerText = timetaken; 
